f5aeac28d9a9e5f0c03101bd65d2450a2aeba582
Natallia Shautsova <65852635+natasha-shev@users.noreply.github.com>
2023-09-20 13:21:51 +0000

    fizzbuzz

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,21 @@
 <?php
+function fizzBuzz() {
+  for ($i = 1; $i < 101; $i++) {
+    $item = '';
+
+    if ($i % 3 === 0) {
+      $item .= 'Fizz';
+    }
+
+    if ($i % 5 === 0) {
+      $item.= 'Buzz';
+    }
+
+    echo strlen($item) ? $item : $i;
+    echo '<br/>'; 
+  }
+}
+
+fizzBuzz();
+
+?>
