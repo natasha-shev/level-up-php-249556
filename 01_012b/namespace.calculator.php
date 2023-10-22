d478d717861c4c1a49fec45cd9c9b20bc2df8c37
Natallia Shautsova <65852635+natasha-shev@users.noreply.github.com>
2023-10-22 20:05:23 +0000

    calculator

--- 01_012b/namespace.calculator.php
+++ 01_012b/namespace.calculator.php
@@ -1,25 +1,38 @@
 <?php
+namespace calculator;
+
+use function calculator\multiply as CalculatorMultiply;
 
 function add($numbers)
 {
+  return array_sum($numbers);
 }
 
 function subtract($start, $numbers)
 {
+  return $start - array_sum($numbers);
 }
 
 function multiply($numbers)
 {
+  return array_product($numbers);
 }
 
 function divide($start, $numbers)
 {
+  return $start / array_product($numbers);
 }
 
 function remainder($start, $numbers)
 {
+  foreach ($numbers as $number) {
+    $start = $start % $number;
+  }
+
+  return $start;
 }
 
 function average($numbers)
 {
+  return array_sum($numbers)/count($numbers);
 }
